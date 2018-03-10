7f2d5c4a5b298b2b7243faafe03773e19ce0c589
Tony Pai <tony77794@gmail.com>
2018-03-10 18:09:19 +0800

    Add fasd support
    Extract zsh configs

---
+++ zsh/aliases.zsh
@@ -0,0 +1,4 @@
+alias ga='git add -A'
+alias gs='git status'
+alias gc='git commit'
+alias gl='git log --graph --date=short'
---
+++ zsh/fasd.zsh
@@ -0,0 +1,6 @@
+alias a='fasd -a' # any
+alias s='fasd -si' # show / search / select
+alias d='fasd -d' # directory
+alias f='fasd -f' # file
+alias z='fasd_cd -d' # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # interactive directory jump
