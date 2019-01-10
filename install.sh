f27e0664afbb141d112bdfe1d382e9678125a188
Tony Pai <tony77794@gmail.com>
2019-01-10 14:29:04 +0800

    Add nvm install before

--- install.sh
+++ install.sh
@@ -22,6 +22,17 @@ if ! which git &> /dev/null; then
   brew install git
 fi
 
+if ! which node &> /dev/null; then
+  echo "🔯 Install node"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+  nvm install --lts
+  nvm ls
+fi
+
 if [ ! -d "$HOME/$ROOT_DIR" ]; then
   echo "🌱 Install dotfiles for the first time"
 
