f9f796980882a1945f93091e36b1f512274b85df
Tony Pai <tony77794@gmail.com>
2020-02-11 11:04:16 +0800

    Add TJ's NVM

--- install.sh
+++ install.sh
@@ -24,13 +24,16 @@ fi
 
 if ! which node &> /dev/null; then
   echo "🔯 Install node"
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+  brew install n
+  n lts
 
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-
-  nvm install lts/carbon
-  nvm ls
+  # curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+  #
+  # export NVM_DIR="$HOME/.nvm"
+  # [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  #
+  # nvm install lts/carbon
+  # nvm ls
 fi
 
 if [ ! -d "$HOME/$ROOT_DIR" ]; then
