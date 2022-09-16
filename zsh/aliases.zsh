fb65ab7dc0a0fd13183532024a6649e4875c4291
Tony Pai <tony77794@gmail.com>
2022-09-16 17:50:18 +0800

    Add cidr alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -105,6 +105,7 @@ alias kl='kubectl logs'
 # shortcut
 alias icloud='cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/'
 alias port='if [ "$(uname)" = "Darwin" ]; then lsof -Pni4 | grep LISTEN; else netstat -tulpn | grep LISTEN; fi'
+alias cidr='if [ "$(uname)" = "Darwin" ]; then ifconfig | grep "inet "; else eval $(ipcalc -np `ifconfig eth0 | sed -n "s/inet addr:\([^ ]*\).*Mask:\([^ ]*\).*/\1 \2/p"`); echo $NETWORK/$PREFIX; fi'
 
 # instant snippets
 alias q='cd ~/.local/share/instant-snippets; vim +Rg; cd -'
