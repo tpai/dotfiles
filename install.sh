a8f5053af31aaa4b14436b320a9f8f93f68e028d
Tony Pai <tony77794@gmail.com>
2024-06-08 14:41:32 +0800

    Update nvm lts version

--- install.sh
+++ install.sh
@@ -31,8 +31,8 @@ if ! type node &> /dev/null; then
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-  nvm install lts/gallium
-  nvm use lts/gallium
+  nvm install lts/iron
+  nvm use lts/iron
   nvm ls
 fi
 
